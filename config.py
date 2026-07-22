5e1441e6d3a05521a565f1e81df09b7953a99f5d
vishnu kant <vishnutungal8126@gmail.com>
2026-07-22 12:01:03 -0600

    Fix config for Render deployment

--- config.py
+++ config.py
@@ -1,41 +1,23 @@
 import os
-import streamlit as st
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from settings import MODEL_NAME,TEMPERATURE
+from settings import MODEL_NAME, TEMPERATURE
 
 load_dotenv()
-
-def get_api_key(key_name:str):
-    """
-    Read API key from .env(local)
-    or streamlit secrets(deployment)
-    """
-    value = os.getenv(key_name)
-    if value:
-        return value
-    return st.secrets[key_name]
 
 # Read OpenRouter API Key
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
 if not OPENROUTER_API_KEY:
-    OPENROUTER_API_KEY = st.secrets["OPENROUTER_API_KEY"]
+    raise ValueError("OPENROUTER_API_KEY environment variable is missing.")
 
-# Read Tavily API Key
-
-TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
-
-if not TAVILY_API_KEY:
-    TAVILY_API_KEY = st.secrets["TAVILY_API_KEY"]
+# Read Tavily API Key (Optional)
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY", "")
 
 # Create LLM
-
 llm = ChatOpenAI(
-    base_url = "https://openrouter.ai/api/v1",
-    api_key = OPENROUTER_API_KEY,
-    model = MODEL_NAME,
-    temperature = TEMPERATURE
+    base_url="https://openrouter.ai/api/v1",
+    api_key=OPENROUTER_API_KEY,
+    model=MODEL_NAME,
+    temperature=TEMPERATURE
 )
-
-
